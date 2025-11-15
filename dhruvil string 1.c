3e0710b0b4ed5029e5b72946394d28479ee653b7
dhruvildandur08-ops <dhruvildandur08@gmail.com>
2025-11-15 18:44:35 +0530

    String 1
    Add a C program to calculate the length of a string

--- dhruvil string 1.c
+++ dhruvil string 1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    scanf("%[^\n]", str);
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    printf("%d", length);
+    return 0;
+}
 
